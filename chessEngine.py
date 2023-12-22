5eeba47dd1a7c6bf17a6b9e6122142e4ab290e00
Vedant Karale <vedantoff.01@gmail.com>
2023-12-22 10:42:27 +0530

    add Move() class to make moves and keep track of the current board state along side printing the move notation

--- chessEngine.py
+++ chessEngine.py
@@ -24,4 +24,34 @@ class GameState():
         ]
         self.whiteToMove = True
         self.moveLog = []
-        pass
+    
+    def makeMove(self,move):
+        self.board[move.start_row][move.start_col] = "--"
+        self.board[move.end_row][move.end_col] = move.piece_moved
+        self.moveLog.append(move)
+        self.whiteToMove = not self.whiteToMove # swap current player
+
+class Move():
+    # map keys (ranks/files) to values (rows/cols)
+
+    ranksToRows = {"1":7,"2":6,"3":5,"4":4,
+                   "5":3,"6":2,"7":1,"8":0}
+    rowsToRanks = {v:k for k,v in ranksToRows.items()} # reverse the above dictionary
+    filesToCols = {"a":0,"b":1,"c":2,"d":3,
+                   "e":4,"f":5,"g":6,"h":7}
+    colsToFiles = {v:k for k,v in filesToCols.items()}
+
+    def __init__(self,start_square,end_square,board):
+
+        self.start_row = start_square[0]
+        self.start_col = start_square[1]
+        self.end_row = end_square[0]
+        self.end_col = end_square[1]
+        self.piece_moved = board[self.start_row][self.start_col]
+        self.piece_captured = board[self.end_row][self.end_col]
+
+    def getChessNotation(self):
+        return self.getRankFile(self.start_row,self.start_col)+self.getRankFile(self.end_row,self.end_col)
+
+    def getRankFile(self,r,c):
+        return self.colsToFiles[c]+self.rowsToRanks[r]
